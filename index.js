af723b70df018152fa30f5547b151a49b305476c
OyukiCV <oyukivel@hotmail.com>
2019-09-09 04:19:29 -0500

    creacion de cli como ejecutable, index como contenedor para llamar a otros archivos, archivo reading donde leo conetnido de un md; modificacion de readme para las pruebas

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const fs=require('fs');
+const path = require ('path');
+const fetch = require('node-fetch');
+const marked = require('marked'); //para extraer links
+const readmeFile = './README.md';//mi readme
+const reading = require('./reading.js')
+const markdownLinkExtractor = require('markdown-link-extractor');
+// const validate = require('validate.js');//archivo para validar
+// const stats = require('stats.js'); //archivo para ver links rotos
+
+
+const extName =()=> {
+    return new Promise((res,rej)=>{
+         if (path.extname(file)==='.md'){
+          fs.readFile(file, 'utf-8', (err, data) => {
+            if (err) 
+               rej(err); 
+            else     
+            res (data);       
+          })
+         }   
+             
+        })
+     .then (data => extIsMd(data) )
+    }
+    module.exports = extName();
+
+ let linksIn= [];
+ 
+ reading(readmeFile)
+    .then(data=>{
+        const linksIn = marked(data);
+
+        linksIn.forEach(Element => {
+        fetch(Element)
+            if (links===200) {
+                return ('Link Correcto')
+                
+            } else if(links === 404) {
+                return ('Ya no existe')
+            }
+            
+        });
+    })
